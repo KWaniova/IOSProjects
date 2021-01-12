a29304f47c56d675a790d3b6b7155f2e32ecb811
Krystyna Waniova <krystyna.waniova@idevband.com>
2021-01-12 17:22:43 +0100

    WeSplitApp

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,23 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Krystyna Waniova on 22/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+struct WeSplitApp_Previews: PreviewProvider {
+    static var previews: some View {
+        Text("Hello, world!")
+    }
+}
